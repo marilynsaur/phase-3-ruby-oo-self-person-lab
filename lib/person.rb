96f5a245ef68e56a0010bf38681b0d4d41095473
Marilyn Wren <Marilynwren64@yahoo.com>
2022-09-27 10:12:19 -0400

    commit tree aa535d085e70ada5a8b93d9bd66cdd1558dabf74

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,31 @@
 # your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :happiness, :hygiene ,:bank_account
+    attr_writer :bank_account
+    
+    
+
+
+    def initialize(name)
+      @name = name
+      @bank_account = 25
+      @happiness = 8
+      @hygiene = 8
+     
+    end
+   
+   
+    
+   
+  end
+
+
+  
+ 
+#   fido = Dog.new("Fido")
+# fido.get_adopted("Sophie")
+# fido.name_bank_account("$25")
+ 
+       
+  
